0b3b3ab71b79dd2fa251ce5cb07a51b6219e0435
Ryan Campbell <ryacampbell15@gmail.com>
2025-03-18 13:15:22 -0500

    nutritionInfo class

--- nutritionixApiDataManipulation.py
+++ nutritionixApiDataManipulation.py
@@ -1,12 +1,17 @@
 import json
-
+from NutritionInfoClass import NutritionInfo
 
 def manipulate(jsonDictDump):
     for foods in jsonDictDump['foods']:
-        print(f"""
-                Food: {foods['food_name']}
-                Calories: {foods['nf_calories']}
-                Sodium: {foods['nf_sodium']}
-                Sugar: {foods['nf_sugars']}
-                Protein: {foods['nf_protein']}
-              """)
+        nutInfo = NutritionInfo(
+            foods['food_name'],
+            foods['serving_qty'],
+            foods['nf_calories'],
+            foods['nf_total_fat'],
+            foods['nf_cholesterol'],
+            foods['nf_sodium'],
+            foods['nf_total_carbohydrate'],
+            foods['nf_dietary_fiber'],
+            foods['nf_sugars'],
+            foods['nf_protein']
+        )
